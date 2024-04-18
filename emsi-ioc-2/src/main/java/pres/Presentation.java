736f8d35a0c09835cc84939256b20eb9c882349a
Aymanelakrari2001 <128264625+Aymanelakrari2001@users.noreply.github.com>
2024-04-18 14:53:55 +0100

    Add files via upload

---
+++ emsi-ioc-2/src/main/java/pres/Presentation.java
@@ -0,0 +1,14 @@
+package pres;
+
+import ext.DaoImpl2;
+import metier.IMetierImpl;
+
+public class Presentation {
+    public static void main(String[] args) {
+        DaoImpl2 dao=new DaoImpl2();
+        IMetierImpl metier = new IMetierImpl(dao);
+        //metier.setDao(dao);
+        System.out.println("Resultat="+metier.calcul());
+
+    }
+}
